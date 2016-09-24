a5dc16c4586204129f37f1523344dd1d04982e56
katsuramaki <llxo2_5oxll@icloud.com>
2016-09-24 18:28:59 +0900

    簡易フレームワーク作成完了

---
+++ app/Router.php
@@ -0,0 +1,6 @@
+<?php
+use FOO\app\Router as Router;
+$app = new Router();
+
+
+$app->get("/","index");
---
+++ app/controller.php
@@ -0,0 +1,8 @@
+<?php
+namespace FOO\app;
+
+class controller{
+	public function index(){
+		echo "<h1>hello</h1>";
+	}
+}
